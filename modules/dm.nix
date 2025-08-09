b43ef8b1231c56da71fe59e921eed877f7c9d21c
Oséias Batista Pegado <oseias.batista.dev@gmail.com>
2025-08-09 18:25:06 -0300

    Update dm.nix to disable X server, enable Xwayland, and configure SDDM for Wayland; remove kde.nix as its settings are now integrated into dm.nix.

--- modules/dm.nix
+++ modules/dm.nix
@@ -4,7 +4,12 @@
   hardware.graphics.enable32Bit = true;
   hardware.graphics.enable = true;
 
-  services.xserver.enable = true;
+  services.xserver.enable = false;
+
+  programs.xwayland.enable = true;
+
   services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+
   services.desktopManager.plasma6.enable = true;
 }
